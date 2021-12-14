2174192bbb0ae4af1c3b736b4dfeeaf6bec17841
HenriqueHartmann <alemaohartmann@gmail.com>
2021-12-14 11:00:10 -0300

    Add weather_repo

---
+++ lib/repositories/weather_repo.dart
@@ -0,0 +1,23 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:weather_app/models/weather_model.dart';
+
+class WeatherRepo {
+  Future<WeatherModel> getWeather(String city) async {
+    final result = await http.Client().get("api.openweathermap.org/data/2.5/weather?q=$city&APPID=24e5518adced091b9c6413f750c7bec7");
+
+    if (result.statusCode != 200) {
+      throw Exception();
+    }
+
+    return parsedJson(result.body);
+  }
+
+  WeatherModel parsedJson(final response) {
+    final jsonDecoded = json.decode(response);
+    final jsonWeather = jsonDecoded["main"];
+
+    return WeatherModel.fromJson(jsonWeather);
+  }
+}
