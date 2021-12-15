8118aee3572359b34f17c1d80c255d12d7be11a4
HenriqueHartmann <alemaohartmann@gmail.com>
2021-12-15 01:47:24 -0300

    Add https to getWeather url

--- lib/repositories/weather_repo.dart
+++ lib/repositories/weather_repo.dart
@@ -5,7 +5,7 @@ import 'package:weather_app/models/weather_model.dart';
 
 class WeatherRepo {
   Future<WeatherModel> getWeather(String city) async {
-    final result = await http.Client().get("api.openweathermap.org/data/2.5/weather?q=$city&APPID=24e5518adced091b9c6413f750c7bec7");
+    final result = await http.Client().get("https://api.openweathermap.org/data/2.5/weather?q=$city&APPID=24e5518adced091b9c6413f750c7bec7");
 
     if (result.statusCode != 200) {
       throw Exception();
